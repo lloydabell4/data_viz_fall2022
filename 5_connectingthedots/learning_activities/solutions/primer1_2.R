aa0314b5e8f07e4a1587e66ce3b91201385f8f84
Your Name <lloydabell4@gmail.com>
2022-09-21 15:35:25 -0400

    class 5

---
+++ 5_connectingthedots/learning_activities/solutions/primer1_2.R
@@ -0,0 +1,16 @@
+tuition_income <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_income.csv') 
+
+#create code required to build this table
+df <- tuition_income%>%
+  group_by(year)%>%
+  summarise(mean_price = mean(total_price))
+
+#create code required to build this visualization
+tuition_income%>%
+  group_by(year, state, income_lvl)%>%
+  summarise(mean_price = mean(total_price),
+            mean_cost = mean(net_cost))%>%
+  ggplot(aes(year, mean_cost, fill = income_lvl))+
+  geom_col(position = "dodge")
+
+#critique this visualization
